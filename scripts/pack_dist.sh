ee0c7053cb4ec8689b6fd4061ca113e23d6eb6c7
QuentinVB <vanbutsele@intechinfo.fr>
2017-05-24 17:44:08 +0200

    added ci; restored architecture

---
+++ scripts/pack_dist.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    SEMVER=$line
+done < $TRAVIS_BUILD_DIR/version.txt
+VERSION="v$SEMVER-${TRAVIS_BUILD_NUMBER}"
+
+zip -r dist $TRAVIS_BUILD_DIR/src/ITI.PrimarySchool.WebApp/wwwroot/dist
+echo -e "zipped dist"
+ls
+mkdir artifacts
+ls
+cp -R dist.zip $TRAVIS_BUILD_DIR/artifacts/
