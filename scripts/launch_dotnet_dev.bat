fedb8c6af5ce93b7f6ab9aeb13543cdfdfa6c911
QuentinVB <vanbutsele@intechinfo.fr>
2017-06-16 15:02:54 +0200

    cleaned merge

--- scripts/launch_dotnet_dev.bat
+++ scripts/launch_dotnet_dev.bat
@@ -3,8 +3,6 @@ echo [45m[37mLostTime .NET server[0m
 cd ..\src\LostTimeWeb.WebApp
 set ASPNETCORE_ENVIRONMENT=Development
 echo Environnement set to %ASPNETCORE_ENVIRONMENT%
-echo [45m[37m.Net Package Restore...[0m
-dotnet restore
 echo [45m[37m.Net Run app...[0m
 dotnet run
 exit
